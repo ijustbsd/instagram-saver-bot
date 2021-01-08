dcbbe261c2c2a55002f1ac3c861139fa7d992320
Sergey Baboshin <ijustbsd@gmail.com>
2021-01-08 12:15:53 +0300

    Add user-agent for instalooter

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -1,4 +1,9 @@
 #!bin/sh
 
+USER_AGENT="Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0"
+
+mkdir -p ~/.cache/instalooter/2.4.4/
+echo -n $USER_AGENT > ~/.cache/instalooter/2.4.4/user-agent.txt
+
 cd /app
 python3 main.py
